7b3e1f9f2bdf93fcfb76e9042e646054b80ca416
taka1214 <globar173@gmail.com>
2023-03-22 15:28:10 +0900

    laravelBreeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TroublePostController;
-use App\Http\Controllers\ReplyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,22 +9,23 @@ use App\Http\Controllers\ReplyController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::resource('posts', TroublePostController::class);
-
-Route::get('/replies', [ReplyController::class, 'index'])->name('replies.index');
-Route::get('/replies/create/{troublePost}', [ReplyController::class, 'create'])->name('replies.create');
-Route::post('/replies', [ReplyController::class, 'store'])->name('replies.store');
-Route::get('/replies/{reply}/edit', [ReplyController::class, 'edit'])->name('replies.edit');
-Route::put('/replies/{reply}', [ReplyController::class, 'update'])->name('replies.update');
-Route::delete('/replies/{reply}', [ReplyController::class, 'destroy'])->name('replies.destroy');;
-
-// Route::resource('replies', ReplyController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
